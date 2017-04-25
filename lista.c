926711ae545ca884e8ea2ccb276506de3f94badb
Lucas Moutinho <lucasmoutinho>
2017-04-24 21:32:06 -0300

    add func insereinicio

--- lista.c
+++ lista.c
@@ -11,10 +11,23 @@ typedef struct lista{
 	t_elemento* fim;
 } t_lista;
 
+int insereinicio(int valor, t_lista* l){
+	t_elemento* nv;
 
+	nv = (t_elemento*)malloc(sizeof(t_elemento)); 
+	nv -> dado = valor;
+	nv -> proximo = l -> inicio;
+	l -> inicio = nv;
+	if(l -> fim == NULL){
+		l -> fim = nv;
+	} 
+	return 0;
+}
 
 
 int main(){
+	t_lista teste;
 
+	
 	return 0;
 }
