85e1f3282b8ef8ef308ffb7de969ad7edb6f331c
Lucas Moutinho <lucasmoutinho>
2017-04-24 21:09:31 -0300

    criado arquivo lista

---
+++ lista.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct elemento{
+	int dado;
+	elemento* proximo;
+} t_elemento;
+
+typedef struct lista{
+	t_elemento* inicio;
+	t_elemento* fim;
+} t_lista;
+
+
+
+
+int main(){
+
+	return 0;
+}
