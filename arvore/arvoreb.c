727a1bc2d3cc793947760fcde0a2637026a04107
Lucas Moutinho <lucasmoutinho>
2017-04-30 23:16:53 -0300

    Criado func insere

--- arvore/arvoreb.c
+++ arvore/arvoreb.c
@@ -15,7 +15,31 @@ t_no* criaArvore(int raiz){
 	return no; 
 }
 
-insere 
+int insere(int v, t_no* r){
+	if(r == NULL || r->raiz == v){
+		printf("Erro, arvore vazia ou elemento ja inserido\n");
+		return -1;
+	}
+	if(v > r->raiz){
+		if(r->dir == NULL){
+			r->dir = (t_no*)malloc(sizeof(t_no));
+		}
+		else{
+			insere(v,r->dir);
+		}
+	}
+	else{
+		if(r->esq == NULL){
+			r->esq = (t_no*)malloc(sizeof(t_no));
+		}
+		else{
+			insere(v,r->esq);
+		}
+	}
+	return 0;
+}
+
+
 
 int main(){
 	return 0;
